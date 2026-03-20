b428854ba02619c6f1a260b63f3ec52de72ca1bc
Antonio Baltic <antoniobaltic@icloud.com>
2026-03-20 16:46:06 +0100

    Initial Commit

---
+++ embargo/embargoApp.swift
@@ -0,0 +1,17 @@
+//
+//  embargoApp.swift
+//  embargo
+//
+//  Created by Antonio Baltic on 20.03.26.
+//
+
+import SwiftUI
+
+@main
+struct embargoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
